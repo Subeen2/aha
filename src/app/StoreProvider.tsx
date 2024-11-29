9557be8ce3534dc29d6eec92a8dd244aa97cdbe0
Subeen2 <snb0768@naver.com>
2024-11-29 17:58:57 +0900

    공식문서 보고 다시 MUI 적용

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useRef } from "react";
 import { Provider } from "react-redux";
-import { AppStore, makeStore } from "./lib/store";
+// import { AppStore, makeStore } from "./lib/store";
 
 // Redux 스토어와 상호작용하는 모든 구성 요소(생성, 제공, 읽기 또는 쓰기)는 클라이언트 구성 요소여야 합니다.
 // 스토어에 액세스하려면 React 컨텍스트가 필요하고 컨텍스트는 클라이언트 구성 요소에서만 사용할 수 있기 때문
@@ -15,14 +15,13 @@ export default function StoreProvider({
   user: any;
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>();
-  if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    storeRef.current = makeStore();
+  // const storeRef = useRef<AppStore>();
+  // if (!storeRef.current) {
+  // Create the store instance the first time this renders
+  // storeRef.current = makeStore();
 
-    // TODO :: 디스패치 안 매개변수 고쳐야 함.
-    storeRef.current.dispatch(() => {});
-  }
+  // TODO :: 디스패치 안 매개변수 고쳐야 함.
+  // storeRef.current.dispatch(() => {});
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return <></>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-
 import Footer from "@/widgets/ui/Footer";
 import Header from "@/widgets/ui/Header";
 import TopBandBanner from "@/widgets/ui/TopBandBanner";
@@ -10,9 +8,9 @@ import Head from "next/head";
 import ClientLayout from "./ClientLayout";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] }); // app/layout.js
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 
-const theme = createTheme();
+const inter = Inter({ subsets: ["latin"] }); // app/layout.js
 
 export const metadata: Metadata = {
   title: "AHA",
@@ -31,13 +29,13 @@ const RootLayout = ({
 }>) => {
   // TODO :: 일단 header, footer 여기에 두고 페이지 별로 헤더 푸터 유동적으로 사용해야 하는지 판단 후 구조 변경
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <html lang="en">
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <body className={inter.className}>
+    <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <body className={inter.className}>
+        <AppRouterCacheProvider>
           <ClientLayout>
             <TopBandBanner
               title={"오늘의 추천 키워드"}
@@ -48,9 +46,9 @@ const RootLayout = ({
             {children}
           </ClientLayout>
           <Footer />
-        </body>
-      </html>
-    </ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
   );
 };
 
