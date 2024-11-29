27e42dd7aec325f5136bcda9767a89377d889a16
Subeen2 <snb0768@naver.com>
2024-11-29 17:49:33 +0900

    mui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+
 import Footer from "@/widgets/ui/Footer";
 import Header from "@/widgets/ui/Header";
 import TopBandBanner from "@/widgets/ui/TopBandBanner";
@@ -8,7 +10,9 @@ import Head from "next/head";
 import ClientLayout from "./ClientLayout";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] }); // app/layout.js
+
+const theme = createTheme();
 
 export const metadata: Metadata = {
   title: "AHA",
@@ -27,23 +31,26 @@ const RootLayout = ({
 }>) => {
   // TODO :: 일단 header, footer 여기에 두고 페이지 별로 헤더 푸터 유동적으로 사용해야 하는지 판단 후 구조 변경
   return (
-    <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body className={inter.className}>
-        <ClientLayout>
-          <TopBandBanner
-            title={"오늘의 추천 키워드"}
-            contentArr={["MVVM 패턴", "Flex 패턴"]}
-            isRandom={true}
-          />
-          <Header />
-          {children}
-        </ClientLayout>
-        <Footer />
-      </body>
-    </html>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body className={inter.className}>
+          <ClientLayout>
+            <TopBandBanner
+              title={"오늘의 추천 키워드"}
+              contentArr={["MVVM 패턴", "Flex 패턴"]}
+              isRandom={true}
+            />
+            <Header />
+            {children}
+          </ClientLayout>
+          <Footer />
+        </body>
+      </html>
+    </ThemeProvider>
   );
 };
 
